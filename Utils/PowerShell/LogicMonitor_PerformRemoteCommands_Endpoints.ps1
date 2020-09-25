4f962d298167ab933f50c196b1c6a8f6ea47eb7a
Vitor Santos <63666988+vitor7santos@users.noreply.github.com>
2020-09-25 15:46:50 -0400

    Create LogicMonitor_PerformRemoteCommands_Endpoints.ps1
    Adding this useful portion of code (to perform remote calls to endpoints - WinPS)

---
+++ Utils/PowerShell/LogicMonitor_PerformRemoteCommands_Endpoints.ps1
@@ -0,0 +1,65 @@
+﻿#retrieving required properties
+$deviceName = "##system.displayname##"
+$username = "##wmi.user##"
+$password = "##wmi.pass##"
+
+$secpw = ConvertTo-SecureString $password -AsPlainText -Force
+$cred  = New-Object Management.Automation.PSCredential ($username, $secpw)
+
+$scriptToExecute =
+{
+    # FUNCTION AREA
+    function SpecialCharacterCheck() {
+        # According to LM the Wildvalue(s) can't contain several characters
+        # -> https://www.logicmonitor.com/support/logicmodules/datasources/active-discovery/script-active-discovery
+        # Replacing  ['=',':','\','#','space'] characters
+
+        Param(
+            [Parameter(position = 0, Mandatory = $true)]
+            [string]$wildvalue
+        )
+        #iterate through the $password in order to find possible reserved characters
+        #WIKI -> https://tools.ietf.org/html/rfc3986#section-2.2
+        if ($wildvalue -match '\='){
+            #replace the '@' for its actual encoding representation
+            $wildvalue = $wildvalue -replace '\=', ''
+        }
+        if ($wildvalue -match '\:'){
+            #replace the '@' for its actual encoding representation
+            $wildvalue = $wildvalue -replace '\:', ''
+        }
+        if ($wildvalue -match '\\'){
+            #replace the '@' for its actual encoding representation
+            $wildvalue = $wildvalue -replace '\\', ''
+        }
+        if ($wildvalue -match '\#'){
+            #replace the '@' for its actual encoding representation
+            $wildvalue = $wildvalue -replace '\#', ''
+        }
+        if ($wildvalue -match '\ '){
+            #replace the '@' for its actual encoding representation
+            $wildvalue = $wildvalue -replace '\ ', ''
+        }
+        if ($wildvalue -match '\.'){
+            #replace the '@' for its actual encoding representation
+            $wildvalue = $wildvalue -replace '\.', ''
+        }
+        Return $wildvalue
+    }
+    # END FUNCTION AREA
+    
+    # <REMOTE CODE HERE>
+
+    # write the desired outcome to the output
+}
+
+# check if wmi.user/pass retrieved value(s)
+if ($username -and $password) {
+    # if yes, impersonate the user in question
+    $result = Invoke-Command -ScriptBlock $scriptToExecute -ComputerName "${deviceName}" -credential $cred
+    write $result
+}else {
+    # if not, tries the test anyway (using the actual user that collector runs with)
+    $result = Invoke-Command -ScriptBlock $scriptToExecute -ComputerName "${deviceName}"
+    write $result
+}
